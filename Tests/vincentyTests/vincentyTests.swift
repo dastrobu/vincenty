b1919d01a348092de44f8220f49c4f5143f2c952
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2018-04-01 19:22:40 +0200

    added tests for nearly antipodal points

--- Tests/vincentyTests/vincentyTests.swift
+++ Tests/vincentyTests/vincentyTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import vincenty
+import func geodesic.distance
 
 // some constants
 private let zero = (lat: 0.0, lon: 0.0)
@@ -25,67 +26,113 @@ final class VincentyTests: XCTestCase {
 
     func testShortcutForEqualPoints() {
         // make sure, points are equal and not identical, to check if the shortcut works correctly
-        XCTAssertEqual(try distance(zero, (lat: 0.0, lon: 0.0), maxIter: 1), 0.0)
+        XCTAssertEqual(try vincenty.distance(zero, (lat: 0.0, lon: 0.0), maxIter: 1), 0.0)
         // make sure, the short cut does not work, if points are not equal
-        XCTAssertThrowsError(try distance(zero, (lat: 0.5.asRad, lon: 179.7.asRad), maxIter: 1))
+        XCTAssertThrowsError(try vincenty.distance(zero, (lat: 0.5.asRad, lon: 179.7.asRad), maxIter: 1))
     }
 
+    /// make sure the computation converges
     func testPoles() {
-        XCTAssertNoThrow(try distance((lat: pi / 2, lon: 0), (lat: -pi / 2, lon: 0)))
+        XCTAssertNoThrow(try vincenty.distance((lat: pi / 2, lon: 0), (lat: -pi / 2, lon: 0)))
     }
 
+    /// make sure another ellipsoid can be employed
     func testGrs80() {
-        XCTAssertNoThrow(try distance((lat: pi / 2, lon: 0), (lat: -pi / 2, lon: 0), ellipsoid: grs80))
+        XCTAssertNoThrow(try vincenty.distance((lat: pi / 2, lon: 0), (lat: -pi / 2, lon: 0), ellipsoid: grs80))
     }
 
-    func testVincentyDistance() {
+    /// make sure, the results are within the right ballpark for some constants
+    func testSimpleConstants() {
         let delta = 1e-3
         var x: (lat: Double, lon: Double), y: (lat: Double, lon: Double)
 
         x = (lat: 0.asRad, lon: 0.asRad)
         y = (lat: 0.asRad, lon: 0.asRad)
-        XCTAssertEqual(try! distance(x, y), 0.0, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 0.0, accuracy: delta)
 
         x = (lat: 0.asRad, lon: 0.asRad)
         y = (lat: 1.asRad, lon: 0.asRad)
-        XCTAssertEqual(try! distance(x, y), 110574.389, accuracy: delta)
-        XCTAssert(abs(try! distance(x, y) - 110574.389) < delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 110574.389, accuracy: delta)
 
         x = (lat: 0.asRad, lon: 0.asRad)
         y = (lat: 2.asRad, lon: 0.asRad)
-        XCTAssertEqual(try! distance(x, y), 221149.453, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 221149.453, accuracy: delta)
 
         x = (lat: 0.5.asRad, lon: 0.asRad)
         y = (lat: -0.5.asRad, lon: 0.asRad)
-        XCTAssertEqual(try! distance(x, y), 110574.304, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 110574.304, accuracy: delta)
 
         x = (lat: -0.5.asRad, lon: 0.asRad)
         y = (lat: 0.5.asRad, lon: 0.asRad)
-        XCTAssertEqual(try! distance(x, y), 110574.304, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 110574.304, accuracy: delta)
 
         x = (lat: 0.asRad, lon: 0.asRad)
         y = (lat: 0.asRad, lon: 1.asRad)
-        XCTAssertEqual(try! distance(x, y), 111319.491, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 111319.491, accuracy: delta)
 
         x = (lat: 0.asRad, lon: 0.asRad)
         y = (lat: 0.asRad, lon: 2.asRad)
-        XCTAssertEqual(try! distance(x, y), 222638.982, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 222638.982, accuracy: delta)
 
         x = (lat: 0.asRad, lon: 0.5.asRad)
         y = (lat: 0.asRad, lon: -0.5.asRad)
-        XCTAssertEqual(try! distance(x, y), 111319.491, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 111319.491, accuracy: delta)
 
         x = (lat: 0.asRad, lon: -0.5.asRad)
         y = (lat: 0.asRad, lon: 0.5.asRad)
-        XCTAssertEqual(try! distance(x, y), 111319.491, accuracy: delta)
+        XCTAssertEqual(try! vincenty.distance(x, y), 111319.491, accuracy: delta)
+    }
+
+    /// use geodesic as reference and test vincenty distance.
+    func testAgainstGeodesic() {
+        var x: (lat: Double, lon: Double), y: (lat: Double, lon: Double)
+
+        x = (lat: 0.asRad, lon: 0.asRad)
+        y = (lat: 0.asRad, lon: 1.asRad)
+        let v = try! vincenty.distance(x, y)
+        let g = geodesic.distance(x, y)
+        print("vincenty: \(v), geodesic: \(g), delta: \(fabs(v - g))")
+        XCTAssertEqual(v, g, accuracy: 1e-3)
+    }
+
+    /// nearly antipodal points, see https://en.wikipedia.org/wiki/Vincenty%27s_formulae#Nearly_antipodal_points
+    func testNearlyAntipodalPoints() {
+        var x: (lat: Double, lon: Double), y: (lat: Double, lon: Double)
+
+        x = (lat: 0.asRad, lon: 0.asRad)
+        y = (lat: 0.5.asRad, lon: 179.5.asRad)
+        let v = try! vincenty.distance(x, y)
+        let g = geodesic.distance(x, y)
+        print("vincenty: \(v), geodesic: \(g), delta: \(fabs(v - g))")
+        XCTAssertEqual(v, g, accuracy: 1e-3)
+        XCTAssertEqual(v, 19936288.579, accuracy: 1e-3)
+    }
+
+    /// nearly antipodal points, see https://en.wikipedia.org/wiki/Vincenty%27s_formulae#Nearly_antipodal_points
+    func testFailOnNearlyAntipodalPoints() {
+        var x: (lat: Double, lon: Double), y: (lat: Double, lon: Double)
+
+        x = (lat: 0.asRad, lon: 0.asRad)
+        y = (lat: 0.5.asRad, lon: 179.7.asRad)
+        XCTAssertThrowsError(try vincenty.distance(x, y))
+
+        // we would like to have a good accuracy here, however, this is one of the cases, where vincenty fails.
+//        let v = try! vincenty.distance(x, y)
+//        let g = geodesic.distance(x, y)
+//        print("vincenty: \(v), geodesic: \(g), delta: \(fabs(v - g))")
+//        XCTAssertEqual(v, g, accuracy: 1e-3)
+//        XCTAssertEqual(v, 19944127.421, accuracy: 1e-3)
     }
 
 #if !os(macOS)
     static var allTests = [
+        ("testAgainstGeodesic", testAgainstGeodesic),
+        ("testFailOnNearlyAntipodalPoints", testFailOnNearlyAntipodalPoints),
         ("testGrs80", testGrs80),
+        ("testNearlyAntipodalPoints", testNearlyAntipodalPoints),
         ("testPoles", testPoles),
         ("testShortcutForEqualPoints", testShortcutForEqualPoints),
-        ("testVincentyDistance", testVincentyDistance),
+        ("testSimpleConstants", testSimpleConstants),
     ]
 #endif
 }
